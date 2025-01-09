1d020195e6b3bb07273ba9b8245ae7a261ef502e
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-01-09 09:53:18 -0800

    Plan basic app structure and start Card component

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import '../styles/App.css';
+import Card from "./Card.jsx";
+
+// score + reset button area
+// card area
+
+function App() {
+  return (
+    <>
+      <Card/>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+// score + reset button area
+// card area
+
+function Card() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Card

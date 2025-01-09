aeaf0c14ba3085987cb7c8b906b0224458fc33fc
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-01-09 10:28:45 -0800

    Make App build cards from array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,25 @@ import Card from "./Card.jsx";
 // score + reset button area
 // card area
 
+let cardsArr = [
+  {name: "France", imageInfo: "./france.jpg", id: 0},
+  {name: "French Flag", imageInfo: "./frenchFlag.jpg", id: 1}
+]
+
+// pairs[idx] is the id of the matching card
+// const pairs = [1, 0]
+
 function App() {
-  const [cardsArr, setCardsArr] = useState([{name:"name", imageInfo:"./test.jpg"}]);
+  const [revealedArr, setRevealedArr] = useState([true]);
+  const [completedArr, setCompletedArr] = useState([false, false]);
+
   return (
     <>
-      <Card data={cardsArr[0]} revealed={true}/>
+      <div className="cardHolder">
+        {cardsArr.map((card) => (
+          <Card data={card} key={card.id} revealed={revealedArr[card.id]} completed={completedArr[card.id]}/>
+        ))}
+      </div>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-
 // score + reset button area
 // card area
 
-function Card({ data, revealed }) {
+function Card({ data, revealed, completed }) {
 
   const name = data.name;
   const imageInfo = data.imageInfo;
+  const id = data.id;
 
-  if (revealed) {
+  if (revealed || completed) {
     return (
       <div className="card">
         <h1>{name}</h1>
         <h3>{imageInfo}</h3>
+        <h3>{id}</h3>
       </div>
     );
   }
